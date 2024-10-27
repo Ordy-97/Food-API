faa07bde8fc2639327c5b244358df72a66c1ed72
ordy-97 <donfackordy97@gmail.com>
2024-10-27 22:21:23 +0100

    config.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-export const MONGO_URI = 'mongodb+srv://user_name:your_password@cluster0.c8hs5ga.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+export const MONGO_URI = 'mongodb+srv://first_user:johndoe0@cluster0.c8hs5ga.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 export const APP_KEY_SECRET = 'OUR_KEY_FOOD';
 
